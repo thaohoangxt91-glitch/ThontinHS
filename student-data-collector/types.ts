0280802c6cae5442f934d997717f401203ac826c
thaohoangxt91-glitch <thaohoangxt91@gmail.com>
2026-01-12 21:20:23 +0700

    Add files via upload

---
+++ student-data-collector/types.ts
@@ -0,0 +1,13 @@
+
+export interface Student {
+  id: string;
+  fullName: string;
+  className: string;
+  birthDate: string;
+  createdAt: string;
+}
+
+export interface SheetConfig {
+  spreadsheetId: string;
+  apiKey: string;
+}
